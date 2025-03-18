c431a6d066569e39ffcfc5e712192e5b0fa5801a
RikiHardianto <antiarikihardianto@gmail.com>
2025-03-18 20:07:27 +0700

    Tugas Sesi 6

---
+++ Tugas Sesi 6/Deret 2.py
@@ -0,0 +1,5 @@
+a , b  = 2,3
+for i in range (8) :
+        print(a, end = " ")
+        a,b = b, a+b
+    
---
+++ Tugas Sesi 6/Deret 3.py
@@ -0,0 +1,6 @@
+a , b  = 40,-1
+for i in range (8) :
+        print(a, end = " ")
+        a += b
+        b -= 2
+    
