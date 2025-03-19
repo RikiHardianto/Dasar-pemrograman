44e970b886e597de3420b0dbd2d8277165485cb9
RikiHardianto <antiarikihardianto@gmail.com>
2025-03-19 20:04:18 +0700

    Delete Tugas Sesi 6/Data.py

--- Tugas Sesi 6/Data.py
+++
@@ -1,6 +0,0 @@
-name = "Nugraha", "john", "Jane", "Doe"
-birtdate = "1989-09-13","1990-01-01","1992-02-02","1994-03-03"
-print('No     | Name     | birtdate  |')
-
-for i, (x,y) in enumerate(zip(name,birtdate), start=1):
-    print(f"{i}   | {x:<7} | {y} |")
